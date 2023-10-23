02c4d5cbde7904b68f989dfd4310e6df68095eaa
akmanova.elizaveta <liza17ak@gmail.com>
2023-10-23 15:56:23 +0300

    creating transactions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -29,14 +29,14 @@ public class Main {
         // Создаём 2 потока с использованием Runnable
         final Runnable thread1 = () -> {
             try {
-                execute(bufferedReader, threshold);
+                execute(bufferedReader, threshold, output);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         };
         final Runnable thread2 = () -> {
             try {
-                execute(bufferedReader, threshold);
+                execute(bufferedReader, threshold, output);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -45,9 +45,6 @@ public class Main {
         // Запускаем потоки
         thread1.run();
         thread2.run();
-
-        // Записываем результат в файл
-        writeIntoFile(output, resultStringBuilder.toString());
     }
 
     /**
@@ -64,8 +61,9 @@ public class Main {
      * "отбор" подходящих значений для output стрима
      * @param br Входящий стрим
      * @param threshold Пороговое значение
+     * @param output Название файла с выходным значением
      */
-    private static void execute(final BufferedReader br, final int threshold) throws IOException {
+    private static void execute(final BufferedReader br, final int threshold, final String output) throws IOException {
         if (br == null) {
             return;
         }
@@ -80,12 +78,13 @@ public class Main {
         final int value = Integer.parseInt(line);
         // Проверка выполнения условия для записи в выходящий канал
         if (hasPreviousValue && checkCondition(value - previousValue, threshold)) {
-            resultStringBuilder.append(line).append("\n");
+            // Записываем результат в файл отдельной транзакцией (каждый поток записывает свою строку в файл)
+            writeIntoFileLikeTransaction(output, line);
         }
         // Сетим предыдущее значение
         previousValue = value;
         hasPreviousValue = true;
-        execute(br, threshold);
+        execute(br, threshold, output);
     }
 
     /**
@@ -101,13 +100,15 @@ public class Main {
     }
 
     /**
-     * Запись данных в файл
+     * Запись данных в файл. Метод синхронизирован, чтобы избежать потери данных при записи в файл разными потоками.
+     * В случае ошибки, данная "транзакция" не повлияет на уже существующий файл и "откатится"
      * @param fileName Путь к файлу
      * @param content Строка, которая запишется в файл
      */
-    private static void writeIntoFile(final String fileName, final String content) {
+    private static synchronized void writeIntoFileLikeTransaction(final String fileName, final String content) {
         try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
-            outputStream.write(content.getBytes(StandardCharsets.UTF_8));
+            outputStream.write((resultStringBuilder + content + "\n").getBytes(StandardCharsets.UTF_8));
+            resultStringBuilder.append(content).append("\n");
         } catch (final IOException e) {
             System.out.println("При записи в файл произошла ошибка");
         }
